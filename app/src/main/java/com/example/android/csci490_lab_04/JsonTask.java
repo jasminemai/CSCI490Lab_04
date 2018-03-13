6f10240e2fa12a1bda0a1f4879fd929779d2735d
Jasmine Mai <maijd@g.cofc.edu>
2018-03-13 19:35:51 -0400

    async task

---
+++ app/src/main/java/com/example/android/csci490_lab_04/JsonTask.java
@@ -0,0 +1,57 @@
+package com.example.android.csci490_lab_04;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Jasmine on 3/13/18.
+ */
+
+public class JsonTask extends AsyncTask<String, Void, String>{
+    private TextView text;
+    public JsonTask(TextView textView)
+    {
+        text = textView;
+    }
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Log.i("JSON", s);
+        text.setText(s);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return urlConnection(strings[0]);
+    }
+
+    public String urlConnection(String urlString)
+    {
+        String line = "";
+        try{
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+            InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+            BufferedReader in = new BufferedReader(isr);
+            int i = 0;
+            while(i < 25)
+            {
+                line = in.readLine();
+                i++;
+            }
+            in.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        Log.i("JSON", line);
+        return line;
+    }
+}
--- app/src/main/java/com/example/android/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/android/csci490_lab_04/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.android.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        downloadJson("https://jsonplaceholder.typicode.com/users");
+
+    }
+    public String downloadJson(String url)
+    {
+        JsonTask jsonTask = new JsonTask(textView);
+        jsonTask.execute(url);
+        return "";
     }
 }
